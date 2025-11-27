82628961062b0910c6908c3b25fe62ed1a3e0a5f
Amjoy09 <amjoy1125@gmail.com>
2025-11-27 19:56:10 +0600

    added images dynamically to the services detail section

--- src/pages/ServiceDetails.jsx
+++ src/pages/ServiceDetails.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const ServiceDetails = () => {
-  return <div>I M Sorry. Skipped the static parts.....</div>;
+  const [services, setServices] = useState([]);
+  const { myId } = useParams();
+
+  useEffect(() => {
+    fetch("/services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const findResult = services.find((service) => service.serviceId == myId);
+
+  return (
+    <div>
+      <img src={findResult?.image} alt="" />
+    </div>
+  );
 };
 
 export default ServiceDetails;
